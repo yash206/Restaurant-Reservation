e9717aa9664c069e00b8e3c60d883e3f3561601d
Yash Agarwal <agarwalyash9120076911@gmail.com>
2023-02-22 01:02:48 +0530

    Addes Models

--- BetterEat/reservation/models.py
+++ BetterEat/reservation/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Booking(models.Model):
+    ID = models.IntegerField(primary_key=True)
+    Name = models.CharField(max_length=255)
+    No_of_guests = models.IntegerField()
+    BookingDate = models.DateTimeField()
+    
+    def __str__(self):
+        return str(self.ID)
+    
+class Menu(models.Model):
+    ID = models.IntegerField(primary_key=True)
+    Title = models.CharField(max_length=255)
+    Price = models.DecimalField(max_digits=10,decimal_places=2)
+    Inventory = models.IntegerField()
+    
+    def __str__(self):
+        return str(self.ID)
+    
