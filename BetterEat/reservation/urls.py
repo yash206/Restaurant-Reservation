1e45d611db5a51504b5497aae71aba290d9c9370
Yash Agarwal <agarwalyash9120076911@gmail.com>
2023-02-23 22:29:05 +0530

    started setting up authentication

--- BetterEat/reservation/urls.py
+++ BetterEat/reservation/urls.py
@@ -1,7 +1,9 @@
 from .views import MenuItemsView, SingleMenuItemView
 from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('items/', MenuItemsView.as_view()),
-    path('item/<int:pk>', SingleMenuItemView.as_view()),
+    path('item<int:pk>/', SingleMenuItemView.as_view()),
+    path('api-token-auth/', obtain_auth_token)
 ]
